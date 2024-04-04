1a7b7e0d66b7358a1b13013e6bb91c35f722f5a5
lagostaDEV <recommendation082@gmail.com>
2024-04-04 19:45:32 +0500

    added some request to db

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -6,6 +6,19 @@ class UserController {
         const newUser = await db.query(`INSERT INTO userS (firstname, lastname, loginUser, passwordUser) values ($1, $2, $3, $4) RETURNING *`, [firstname, lastname, loginUser, passwordUser])
         res.send(newUser.rows[0]);
     }
+
+    async getUser(req, res) {
+        const id = req.query.id;
+        const user = await db.query(`SELECT * FROM userS where id = $1`, [id]);
+
+        res.send(user.rows);
+    }
+
+    async getAllUsers(req, res) {
+        const users = await db.query(`SELECT * FROM userS`);
+
+        res.send(users.rows);
+    }
 }
 
 module.exports = new UserController();
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,6 +2,8 @@ const Router = require(`express`);
 const router = new Router();
 const UserController = require(`../controllers/user.controller`);
 
-router.post(`/user`, UserController.createUser);
+router.post(`/create_user`, UserController.createUser);
+router.get(`/get_one_user`, UserController.getUser);
+router.get(`/get_all_users`, UserController.getAllUsers);
 
 module.exports = router;
