a1202a547d9c2ddfb87eebb43d422a0e8f7d47c0
lagostaDEV <recommendation082@gmail.com>
2024-04-13 14:01:21 +0500

    added some logic edit user

--- frontend/src/api/user/editUser.ts
+++ frontend/src/api/user/editUser.ts
@@ -5,18 +5,20 @@ interface Props {
     loginUser: string;
     passwordUser: string;
     avatarUser: string;
+    clear: () => void;
     id: number | undefined;
 }
 
-export function editUser({ loginUser, passwordUser, avatarUser, id }: Props) {
+export function editUser({ loginUser, passwordUser, avatarUser, id, clear }: Props) {
     axios.put(`${api}/api/update_user`, {
         loginUser: loginUser,
         passwordUser: passwordUser,
         avatarUser,
         id: id
-    }).then((resp) => {
-        console.log(resp.data);
-    }).catch((err) => {
-        console.log(err);
+    }).then(() => {
+        location.reload();
+        clear();
+    }).catch(() => {
+        clear();
     })
 }
--- frontend/src/components/Header/ModalHeader/MyAccount/MyAccountBody.tsx
+++ frontend/src/components/Header/ModalHeader/MyAccount/MyAccountBody.tsx
@@ -1,35 +1,80 @@
-import { FormEvent } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { useUserData } from '../../../../store/useUserData';
 import './myAccount.css';
 import { editUser } from '../../../../api/user/editUser';
 import { currentId } from '../../../../api/user/currentId';
 
 export function MyAccountBody() {
+    const [loginUser, setLogin] = useState("");
+    const [avatarUser, setAvatar] = useState("");
+    const [passwordUser, setPassword] = useState("");
+    const [description, setDescription] = useState("");
+
+    function handleChangeAvatar(event: ChangeEvent<HTMLInputElement>) {
+        setAvatar(event.target.value);
+    }
+
+    function handleChandePassword(event: ChangeEvent<HTMLInputElement>) {
+        setPassword(event.target.value);
+    }
+
+    function handleChangeDescription(event: ChangeEvent<HTMLInputElement>) {
+        setDescription(event.target.value);
+    }
+
+    function handleChangeLogin(event: ChangeEvent<HTMLInputElement>) {
+        setLogin(event.target.value);
+    }
+
+    function clear() {
+        setAvatar("");
+        setDescription("");
+        setPassword("");
+        setAvatar("");
+    }
+
     const { userData } = useUserData(currentId());
     const id = userData.id
-    const passwordUser = "qwe";
-    const avatarUser = "govnotestEdit";
-    const loginUser = "lagostaEDIT";
 
     function handleSubmit(event: FormEvent) {
         event.preventDefault();
         event.stopPropagation();
 
-        editUser({ id, passwordUser, avatarUser, loginUser });
+        editUser({ id, passwordUser, avatarUser, loginUser, clear });
     }
 
     return (
         <div className="form-container">
-            <form className="form" onClick={handleSubmit}>
+            <form className="form" onSubmit={handleSubmit}>
                 <div>
                     <input
                         type="text"
                         className="input-modal"
-                        value={userData.loginuser}
+                        placeholder="Enter login"
+                        value={loginUser}
+                        onChange={handleChangeLogin}
                     />
-                    <input type="text" className="input-modal mt-3" placeholder="Enter description" />
-                    <input type="text" className="input-modal mt-3" placeholder="Enter avatar" />
-                    <input type="password" className="input-modal mt-3" placeholder="Enter password" />
+                    <input
+                        type="text"
+                        className="input-modal mt-3"
+                        placeholder="Enter description"
+                        value={description}
+                        onChange={handleChangeDescription}
+                    />
+                    <input
+                        type="text"
+                        className="input-modal mt-3"
+                        placeholder="Enter avatar"
+                        value={avatarUser}
+                        onChange={handleChangeAvatar}
+                    />
+                    <input
+                        type="password"
+                        className="input-modal mt-3"
+                        placeholder="Enter password"
+                        value={passwordUser}
+                        onChange={handleChandePassword}
+                    />
                     <button className="upperCase button-change mt-5">Change</button>
                 </div>
             </form>
